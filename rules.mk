8eeac958c63cbc350e1f2e4a88528c9dd4cf3872
nedsi <nedsi@pm.me>
2020-07-30 22:59:28 +0200

    first commit

---
+++ rules.mk
@@ -0,0 +1,9 @@
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+BACKLIGHT_ENABLE = no
+MOUSEKEY_ENABLE = yes      # Enable mouse keys
+WPM_ENABLE = yes      		 # Enable WPM measurement
+
+EXTRAFLAGS += -flto
+
