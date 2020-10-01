5ec5132dfc9b3225970d5d628a5c5b8e853a3023
nedsi <nedsi@pm.me>
2020-10-01 18:35:01 +0200

    small corrections all around on master

--- rules.mk
+++ rules.mk
@@ -1,17 +1,17 @@
-OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
-ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+OLED_DRIVER_ENABLE = yes   # Enable the use of OLED displays
+ENCODER_ENABLE = yes       # Enable the use of one or more encoders
 RGBLIGHT_ENABLE = no       # Disable keyboard RGB underglow
-WPM_ENABLE = yes     	     # Enable WPM measurement
+WPM_ENABLE = yes           # Enable WPM measurement
 SPLIT_TRANSPORT = mirror   # Allow to have information sent on both sides
 TAP_DANCE_ENABLE = yes     # Activate tap dance
 UNICODEMAP_ENABLE = yes    # Enable unicode map
-COMBO_ENABLE = yes 				 # Enable combos
-NKRO_ENABLE = yes 				 # Enable NKRO: any number of key-presses at once
-LTO_ENABLE = yes  				 # Enable Link Time Optimization
+COMBO_ENABLE = yes         # Enable combos
+NKRO_ENABLE = yes          # Enable NKRO: any number of key-presses at once
+LTO_ENABLE = yes           # Enable Link Time Optimization
 
-SRC += combos.c			       # Source combos.c 
+SRC += combos.c            # Source combos.c 
 
-EXTRFLAGS += -flto
+EXTRFLAGS += -flto         # Save space 
 
 MOUSEKEY_ENABLE = no       # Disable mouse keys
-BACKLIGHT_ENABLE = no     # Enable keyboard backlight (not RGB)
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight (not RGB)
--- tapdance.h
+++ tapdance.h
@@ -3,11 +3,7 @@
 #include QMK_KEYBOARD_H
 
 enum {
-    DOT_TD = 0,
-    XCLM_TD,
     CA_CC_CV,
 };
 
-#define TD_XCLM TD(XCLM_TD)
-#define TD_DOT TD(DOT_TD)
 #define CACCCV TD(CA_CC_CV)
