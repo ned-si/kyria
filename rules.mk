566d3be405a4a4cf4dd37d66acd756b1aa449d26
Julien Sudan <nedsi@pm.me>
2024-02-21 21:05:15 +0100

    refactor: use userspace config

--- rules.mk
+++ rules.mk
@@ -1,13 +1,13 @@
 OLED_DRIVER_ENABLE = yes   # Enable the use of OLED displays
 ENCODER_ENABLE = yes       # Enable the use of one or more encoders
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
 SPLIT_TRANSPORT = mirror   # Allow to have information sent on both sides
 TAP_DANCE_ENABLE = yes     # Activate tap dance
 UNICODEMAP_ENABLE = yes    # Enable unicode map
 COMBO_ENABLE = yes 				 # Enable combos
 NKRO_ENABLE = yes 				 # Enable NKRO: any number of key-presses at once
 LTO_ENABLE = yes  				 # Enable Link Time Optimization
-MOUSEKEY_ENABLE = yes      # Enable mouse keys
+MOUSEKEY_ENABLE = no      # Enable mouse keys
 
 SRC += combos.c			       # Source combos.c 
 
