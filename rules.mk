fb527d9dbaf1110195ec4a70af4e80690bc40c00
Julien Sudan <nedsi@pm.me>
2024-02-21 22:13:11 +0100

    feat: reenable rgb

--- rules.mk
+++ rules.mk
@@ -1,6 +1,6 @@
 OLED_DRIVER_ENABLE = yes   # Enable the use of OLED displays
 ENCODER_ENABLE = yes       # Enable the use of one or more encoders
-RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 SPLIT_TRANSPORT = mirror   # Allow to have information sent on both sides
 TAP_DANCE_ENABLE = yes     # Activate tap dance
 UNICODEMAP_ENABLE = yes    # Enable unicode map
